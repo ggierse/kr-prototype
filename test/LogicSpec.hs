43d4dbdcb5dbe16cb84a5e278f5cd8d1492de024
Gesche Gierse <gesche.gierse@rwth-aachen.de>
2016-11-23 18:19:13 +0100

    add test cases for branch computation

--- test/LogicSpec.hs
+++ test/LogicSpec.hs
@@ -64,6 +64,16 @@ spec = do
             isFixPoint protoUnfixed `shouldBe` False
         it "not an fixpoint because of base" $
             isFixPoint protoUnfixed2 `shouldBe` False
+    describe "getBranchToP0" $ do
+      it "P0 is one step away" $
+        length (getBranchToP0 testKB bikeProto) `shouldBe` 2
+      it "branch order is correct for one steps" $
+        getBranchToP0 testKB bikeProto `shouldBe` [bikeProto, vehicleProto]
+      it "P0 is two steps away" $
+        length (getBranchToP0 testKB carProto) `shouldBe` 3
+      it "branch order is correct for two steps" $
+        getBranchToP0 testKB carProto `shouldBe` [carProto, bikeProto, vehicleProto]
+
     describe "computeFixpoint" $ do
         it "reduce to P0" $
             computeFixpoint testKB bike `shouldBe` bikeFixpoint
