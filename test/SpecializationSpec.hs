fcabb8d1aa241da0205b31933e8a8aaec0954b03
Gesche Gierse <gesche.gierse@rwth-aachen.de>
2016-11-29 17:12:26 +0100

    add specialization test descriptions

--- test/SpecializationSpec.hs
+++ test/SpecializationSpec.hs
@@ -8,6 +8,15 @@ import qualified Data.Set as Set
 
 spec :: Spec
 spec = do
-    describe "isSpecialization" $ do
-        it "placeholder" $
-            True `shouldBe` True
+    describe "changeExpressionIsSpecialization" $ do
+        context "one change expression is a specialization of another if" $ do
+          it "they are equal" $
+            True `shouldBe` False
+          it "the specialized is a subset of the generalization" $
+            True `shouldBe` False
+          it "the specialized fullfills a number constraint exposed by the generalization" $
+            True `shouldBe` False
+    describe "isSpecialization" $
+      context "one PrototypeExpression is a specialization of another if" $
+        it "all properties are a changeExpresionSpecialization" $
+          True `shouldBe` False
