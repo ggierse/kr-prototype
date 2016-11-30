996d2166cbc1f6a05a26f9657495b8e2373f6e0c
Gesche Gierse <gesche.gierse@rwth-aachen.de>
2016-11-30 18:12:18 +0100

    some code for specialization, mostly not tested yet

--- src/Prototype/Specialization.hs
+++ src/Prototype/Specialization.hs
@@ -3,11 +3,70 @@ module Prototype.Specialization where
 import Prototype.Basis
 
 import qualified Data.Set as Set
+import Data.Maybe (isJust, fromJust)
 
 
-changeExpressionIsSpecializationOf :: SimpleChangeExpression -> SimpleChangeExpression -> Bool
-changeExpressionIsSpecializationOf special@(Change propS iriSetS) general@(Change propG iriSetG)
-  | propS /= propG = False
-  | general == special = True
-  | iriSetS `Set.isSubsetOf` iriSetG = True
-  | otherwise = False
+data Constraint = Exactly Int | Atleast Int | Atmost Int deriving (Show, Eq, Ord)
+data ComplexValue = Value IRI | Const Constraint deriving (Show, Eq, Ord)
+
+type ConstraintView = Maybe Constraint
+
+class PossiblyConstraint a where
+  isConstraint :: a -> Bool
+  getConstraint :: a -> Maybe Constraint
+  view :: a -> ConstraintView
+
+
+instance PossiblyConstraint ComplexValue where
+  isConstraint (Const _) = True
+  isConstraint _ = False
+  getConstraint (Const c) = Just c
+  getConstraint _ = Nothing
+  view = getConstraint
+
+class PossiblyIRI a where
+  getIRI :: a -> Maybe IRI
+
+instance PossiblyIRI ComplexValue where
+  getIRI (Value iri) = Just iri
+  getIRI _ = Nothing
+--class Specializable a where
+--  isSpecializationOf :: a -> a -> Bool
+
+class Specializable a b where
+  isSpecializationOf :: a -> b -> Bool
+
+instance Specializable Constraint Constraint where
+  isSpecializationOf (Exactly s) (Exactly g) = s == g
+  isSpecializationOf (Atleast s) (Atleast g) = s >= g
+  isSpecializationOf (Atmost s) (Atmost g) = s <= g
+  isSpecializationOf _ _ = False
+
+instance Specializable ComplexValue ComplexValue where
+  isSpecializationOf (Value s) (Value g) = s == g
+  isSpecializationOf (Const s) (Const g) = s `isSpecializationOf` g
+  isSpecializationOf _ _ = False
+
+instance Specializable (Set.Set IRI) Constraint where
+  isSpecializationOf set (Exactly n) = Set.size set == n
+  isSpecializationOf set (Atmost n) = Set.size set <= n
+  isSpecializationOf set (Atleast n) = Set.size set >= n
+
+getIris :: Set.Set ComplexValue -> Set.Set IRI
+getIris complexSet =
+  let maybeIris = Set.map getIRI complexSet
+      justIris = Set.filter isJust maybeIris
+  in Set.map fromJust justIris
+
+instance (Ord a) => Specializable (ChangeExpression a) (ChangeExpression a) where
+  isSpecializationOf special@(Change propS propSetS) general@(Change propG propSetG)
+    | propS /= propG = False
+    | general == special = True
+    | propSetS `Set.isSubsetOf` propSetG = True
+    | otherwise = False
+
+    {-- für alle properties der generalization muss gelten
+     ach warte das ist komisch für nur eine change expression.
+     Was machen wir überhaupt, wenn wir ein (atLeast 5) und irgendwelche iris haben?
+     -> könnte man interpretieren als atleast 5 und diese hier sind fix
+--}
--- test/SpecializationSpec.hs
+++ test/SpecializationSpec.hs
@@ -1,5 +1,4 @@
 module SpecializationSpec (spec) where
-import Prototype.Basis
 import Prototype.Specialization
 import Test.Hspec
 
@@ -13,16 +12,16 @@ spec = do
     describe "changeExpressionIsSpecialization" $ do
         context "one change expression is a specialization of another if" $ do
           it "they are equal" $
-            changeExpressionIsSpecializationOf changeNameMyName changeNameMyName `shouldBe` True
+            isSpecializationOf changeNameMyName changeNameMyName `shouldBe` True
           it "the specialized is a subset of the generalization" $
-            changeExpressionIsSpecializationOf changeWheelsToTwo changeWheelsTwoFour `shouldBe` True
+            isSpecializationOf changeWheelsToTwo changeWheelsTwoFour `shouldBe` True
           it "the specialized fullfills a number constraint exposed by the generalization" $
             True `shouldBe` False
           it "the specialized fullfills an existance quantification constraint exposed by the generalization" $
             True `shouldBe` False
         context "one change expression is not a specialization of another if" $
           it "they describe a differently named property" $
-            changeExpressionIsSpecializationOf changeNameMyName changeWheelsMyName `shouldBe` False
+            isSpecializationOf changeNameMyName changeWheelsMyName `shouldBe` False
     describe "isSpecialization" $
       context "one PrototypeExpression is a specialization of another if" $ do
         it "all properties are a changeExpresionSpecialization" $
