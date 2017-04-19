e8127d4d1d457644a787886f049890d07236fc6e
Gesche Gierse <gesche.gierse@rwth-aachen.de>
2017-04-19 10:27:12 +0200

    implement remaining specialization strictly from formalization

--- src/Prototype/Specialization.hs
+++ src/Prototype/Specialization.hs
@@ -8,7 +8,7 @@ import qualified Data.Set as Set
 import qualified Data.List as List
 import Debug.Trace
 import Data.Maybe (isJust, fromJust)
-
+import Data.IntegerInterval as Interval
 {--
 Specialization relation
 given prototypes s,g: s isSpecializationOf g if for all prototypes G \in properties(g) it holds that
@@ -31,7 +31,7 @@ existsSpecial fkb sprops g =
   case samePropertyName of
     Nothing -> False
     Just s -> isPropertySpecialization fkb s g
-  where samePropertyName = List.find (propertyIdIsEqual g) $ Set.toList sprops
+  where samePropertyName = List.find (propertyIdIsEqual g) sprops
 
 propertyIdIsEqual :: Prototype IRI -> Prototype IRI -> Bool
 propertyIdIsEqual a b =
@@ -39,9 +39,32 @@ propertyIdIsEqual a b =
 
 isPropertySpecialization :: FixpointKnowledgeBase IRI -> Prototype IRI -> Prototype IRI -> Bool
 isPropertySpecialization fkb s g
-  | Set.null gConsts = val g `Set.isSubsetOf` val s && accountFor s gConsts
+  | Set.null gConsts = val g `Set.isSubsetOf` val s && accountFor fkb s gConsts
   | otherwise = True
   where gConsts = consts fkb g
 
-accountFor :: Prototype IRI -> Set ConstraintInfo -> Bool
-accountFor s gConsts= False
+accountFor :: FixpointKnowledgeBase IRI -> Prototype IRI -> Set ConstraintInfo -> Bool
+accountFor fkb s gConsts
+  | Set.null sConsts = Set.foldl' (\ prev gc -> prev && isSatisfied (val s) gc) True gConsts
+  | otherwise = Set.foldl' (\ prev gc -> prev && isMatched sConsts gc) True gConsts
+  where sConsts = consts fkb s
+
+
+isMatched :: Foldable t1 => t1 ConstraintInfo -> ConstraintInfo -> Bool
+isMatched sConsts gc = isJust $ existsMatching sConsts gc
+
+existsMatching :: Foldable t => t ConstraintInfo -> ConstraintInfo -> Maybe ConstraintInfo
+existsMatching sConsts gc =
+  case gc of
+    CardinalityConst _ _ ->
+      List.find (\sc -> constType sc == constType gc && constInterval sc `Interval.isSubsetOf` constInterval gc) sConsts
+    TypeConst _ _ ->
+      List.find (\sc -> constType sc == constType gc && constValues sc `Set.isSubsetOf` constValues gc) sConsts
+
+
+isSatisfied :: Set IRI -> ConstraintInfo -> Bool
+isSatisfied sVals gc =
+  case constType gc of
+    AllValuesFrom -> Set.foldl' (\ prev v -> prev && v `Set.member` constValues gc) True sVals
+    SomeValuesFrom -> isJust $ List.find (\ v -> v `Set.member` constValues gc) sVals
+    Cardinality -> toInteger (Set.size sVals) `Interval.member` constInterval gc
